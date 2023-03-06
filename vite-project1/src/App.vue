fd5a9a899b67b5f4cda3d22cceacaf2add62403d
Alberto San Martin <alberto.sanmartin@sii.cl>
2023-03-06 17:57:12 -0300

    first commit

---
+++ vite-project1/src/App.vue
@@ -0,0 +1,38 @@
+<!--SFC nos permite separar componentes en un unico archivo-->
+<!--Logica estructura y estilo SFC-->
+<!--El arbol que se genera en el navegador se llama DOM-->
+<script setup>
+    const name = "Vue 3";
+    const styleColor = "color: red;";
+    const active = true;
+    const arrayColores = ["blue", "red", "peru"];
+    const counter = 1;
+</script>
+
+<template>
+  <!--interpolacion de textos, bigotes o llaves. Es una forma de insertar valores a una plantilla-->
+  <h1>Hola {{name.toUpperCase()}}</h1>
+  
+  <!--Se interpreta como texto-->  
+  <h2>{{arrayColores}}</h2>
+  
+  <!--Para no utilizar los bigotes-->
+  <!--Style con bbind-->
+  <h2 :style="styleColor">Hola {{ name.toUpperCase() }}</h2>
+
+  <!-- interpolacion de texto -->
+  <h3>{{ active ? "Estoy activado" : "Estoy desactivado" }}</h3>
+
+  <!--interpolacion con signo peso y llaves-->
+  <h4 :style="`color: ${arrayColores[2]}`">Soy Peru</h4>
+  
+  <!--interpolacion-->
+  <h5>Contador: {{ counter + 1 }}</h5>
+
+</template>
+
+<style>
+ h1 {
+  color: red;
+ }
+</style>
